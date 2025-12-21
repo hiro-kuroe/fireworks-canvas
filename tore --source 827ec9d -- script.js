fa95ffa716450bc19cf835074fd8021d3a63cdc8
hiro-kuroe <mikebokushi@gmail.com>
2025-12-21 21:33:50 +0900

    しだれ花火完成かな。

---
+++ tore --source 827ec9d -- script.js
@@ -0,0 +1,13 @@
+[33m8d8045a[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mmain[m[33m, [m[1;31morigin/main[m[33m, [m[1;31morigin/HEAD[m[33m)[m fix: fireworks fade out
+[33mf191c4f[m tune firework color and gravity
+[33m827ec9d[m Update script.js
+[33m91c9c34[m Update script.js
+[33m0d3d936[m Update script.js
+[33md7b0a25[m Update script.js
+[33m5830c37[m Update script.js
+[33mf252414[m Update script.js
+[33mc3e6104[m Update script.js
+[33m8c6e72b[m Update script.js
+[33m7ae21d1[m Update script.js
+[33m3aacef5[m Update script.js
+[33me58ac71[m add fireworks canvas
