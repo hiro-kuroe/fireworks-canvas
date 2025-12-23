bebfc2318bc747baa17d283354ed6004913e3a96
hiro-kuroe <mikebokushi@gmail.com>
2025-12-23 21:16:22 +0900

    マゼンダ

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ class Particle {
   const t = this.life / 240; // 1 → 0
 
   // hue: 赤(0) → オレンジ(30) → 金(45)
-  const hue = this.hue - (1 - t) * 5;
+  const hue = 120 - (1 - t) * 30;
 
   const light = 60 - (1 - t) * 30;
   const r = 1.5 * t + 0.5;
@@ -128,7 +128,7 @@ let smokes = [];
 
 
 function firework(x, y) {
-  const count = 2000;
+  const count = 1500;
   const baseSpeed = 2.0;
   const baseHue = Math.random() * 360;
 
